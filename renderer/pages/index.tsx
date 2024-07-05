37786ecab5553b7e38d5b47383730c6060c4e875
Joshua Hughes <joshua@plutonus.dev>
2024-07-05 21:58:28 +1000

    style: replaced double quotes with single quotes in index.tsx file
    This commit replaces the use of double quotes with single quotes for string literals in the index.tsx file for code consistency and style guidelines adherence. No changes in the functionality of the code.

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,14 +1,14 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-import Loader from "../comp/meta/Loader";
+import Loader from '../comp/meta/Loader';
 
 export default function ServiceIndex() {
-	const router = useRouter();
+  const router = useRouter();
 
-	useEffect(() => {
-		setTimeout(() => window.ipc.send('openApp', null), 5000);
-	}, [ router ]);
+  useEffect(() => {
+    setTimeout(() => window.ipc.send('openApp', null), 5000);
+  }, [router]);
 
-	return <Loader label="Loading the hub..." />;
+  return <Loader label="Loading the hub..." />;
 }
