407def927084d8a15b21837162485008b028991a
Joshua Hughes <joshua@plutonus.dev>
2024-07-05 21:53:37 +1000

    feat: Added new configuration files
    Added several new configuration files related to webpack, babel, and tailwind. Also made minor formatting changes to existing config files. These changes are essential for the upcoming development tasks and feature additions.

--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  content: [
-    './renderer/pages/**/*.{js,ts,jsx,tsx}',
-    './renderer/comp/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./renderer/pages/**/*.{js,ts,jsx,tsx}', './renderer/comp/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
